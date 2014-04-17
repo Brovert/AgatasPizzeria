5ed318ff853003c91f30b3d7e0f6264e888cf716
Trevor Brown <Trevor.Brown@bdcsoft.com>
2014-04-17 09:09:10 -0500

    Added the acceptance criteria

--- AgatasPizzeria.Api/Pizza.cs
+++ AgatasPizzeria.Api/Pizza.cs
@@ -2,6 +2,10 @@
 
 namespace AgatasPizzeria.Api {
     public class Pizza {
+        public Pizza() {
+            Toppings = new List<string>();
+        }
+
         public string Size { get; set; }
         public List<string> Toppings { get; set; } 
     }
